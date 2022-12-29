433f1907a7d2b11535599b563129eff4a6ef9640
Amanda Skinner <amandamuscha@yahoo.com>
2022-12-29 18:51:16 -0500

    started problem 4

---
+++ ProjectEuler/src/Problem0004.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Problem0004 {
+    public static void main(String[] args) {
+    System.out.println("Test");
+    }
+}
