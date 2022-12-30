f8b97ae0ae90a9cc697a8a09c92f54f491dd0683
Amanda Skinner <amandamuscha@yahoo.com>
2022-12-29 19:19:44 -0500

    Finished Problem0004

--- ProjectEuler/src/Problem0004.java
+++ ProjectEuler/src/Problem0004.java
@@ -4,7 +4,31 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 public class Problem0004 {
+    //Find the largest palindrome of two numbers that is less than N
     public static void main(String[] args) {
-    System.out.println("Test");
+        int n = 800000;//Update to enter max number
+        int maxPal=0;
+        for(int i = 100;i<1000;i++){
+            for(int j = 100;j<1000;j++){
+                if(i*j>=n){break;}
+                String palChk=Integer.toString(i*j);
+                if(isPalindrome(palChk)&&(i*j)>maxPal){
+                    maxPal=(i*j);
+                }
+            }
+        }System.out.println(maxPal);
+    }
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while(left < right)
+        {
+            if(str.charAt(left) != str.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
