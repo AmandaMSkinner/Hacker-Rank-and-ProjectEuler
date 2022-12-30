e2046e2ef4e7ac9d7b61579e94bd052720b404b8
Amanda Skinner <amandamuscha@yahoo.com>
2022-12-30 16:38:51 -0500

    Finished Problem0006

--- ProjectEuler/src/Problem0006.java
+++ ProjectEuler/src/Problem0006.java
@@ -7,6 +7,13 @@ import java.util.regex.*;
 public class Problem0006 {
     //Find the absolute difference between the sum of the squares of the first N natural numbers and the square of the sum.
     public static void main(String[] args) {
-
+        int n = 10;
+        long sumSquares = 0;
+        long squareSum = 0;
+        for(long i=1;i<=n;i++){
+            sumSquares=sumSquares+(i*i);
+            squareSum=squareSum+i;
+        }squareSum*=squareSum;
+        System.out.println(Math.abs(sumSquares-squareSum));
     }
 }
