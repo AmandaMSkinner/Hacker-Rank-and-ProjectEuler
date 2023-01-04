22d5bf906bdc193c7f803e8005153b92b4a49dc4
Amanda Skinner <amandamuscha@yahoo.com>
2023-01-03 20:11:21 -0500

    Started Problem0012

---
+++ ProjectEuler/src/Problem0012.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Problem0012 {
+    //What is the first triangle number to have over N divisors?
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
